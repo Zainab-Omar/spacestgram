4b5f9e8ca5b4f0d4016da06a075dc9042f110056
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-19 11:16:47 -0500

    delete some data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import './App.css';
+import axios from "axios";
+import React, { useState } from 'react'
 
 const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.React_APP_KEY}`
 
 function App() {
-
   return (
     <div className="App">
     Hello React
