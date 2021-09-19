7c41e4fbb4f3d62287f10ec16654f6d18221b83e
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-19 13:08:38 -0500

    map through images and send each image to Image component

--- src/ImagesDisplay.js
+++ src/ImagesDisplay.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import Image from './Image'
 
 function ImagesDisplay({images}){
-  
+    const imagesArray = images || []
+    const renderImages = imagesArray.map(image => <Image key={uuidv4()} image = {image} />)
 
     return (
         <div>
-          
+          {renderImages}
         </div>
     )
 
